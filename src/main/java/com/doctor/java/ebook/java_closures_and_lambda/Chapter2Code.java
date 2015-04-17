c4e76daf9357a204f139755a67a404c70a78a3ed
sdcuike <sdcuike@users.noreply.github.com>
2015-04-17 18:06:36 +0800

    ebook.java_closures_and_lambda Chapter2Code

---
+++ src/main/java/com/doctor/java/ebook/java_closures_and_lambda/Chapter2Code.java
@@ -0,0 +1,42 @@
+package com.doctor.java.ebook.java_closures_and_lambda;
+
+import java.util.function.Function;
+import java.util.function.Supplier;
+
+import com.google.common.base.Preconditions;
+
+/**
+ * ebook.java_closures_and_lambda Chapter2Code
+ * 
+ * @author doctor
+ *
+ * @time 2015年4月17日 下午6:05:25
+ */
+
+public class Chapter2Code {
+
+	public static void main(String[] args) {
+
+		// Object funObject = e -> e; //error
+		// Function<?, ?> function = e -> e; //范型 error
+		Function<Number, Number> functionNumber = e -> e;
+		Preconditions.checkArgument(functionNumber.apply(66).equals(66));
+
+		Supplier<String> supplier = () -> "hello";
+		Preconditions.checkState("hello".equals(supplier.get()));
+
+		// Using Explicit Typing to Resolve Overloaded Method Ambiguity
+		// -> String str 消除重载问题
+		System.out.println(transform("hello", (String str) -> str + "----"));
+		// P22
+
+	}
+
+	private static CharSequence transform(CharSequence charSequence, Function<CharSequence, CharSequence> transformer) {
+		return transformer.apply(charSequence);
+	}
+
+	private static String transform(String string, Function<String, String> transformer) {
+		return transformer.apply(string);
+	}
+}
