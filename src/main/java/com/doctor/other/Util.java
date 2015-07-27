60ea49469e6cf59fe1d2b790075895584c63e97d
sdcuike <sdcuike@users.noreply.github.com>
2015-07-27 15:22:06 +0800

    add Util

---
+++ src/main/java/com/doctor/other/Util.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2014-present  The  openSourcLibrary-2015  Authors
+ *
+ * https://github.com/sdcuike
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.doctor.other;
+
+import java.time.format.DateTimeFormatter;
+
+/**
+ * @author doctor
+ *
+ * @time 2015年7月27日 下午3:01:08
+ */
+public final class Util {
+	public static final DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("YYYYMMdd");
+}
